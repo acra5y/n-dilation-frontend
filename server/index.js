43a0a45df9135869674328b9b937aa69b889a39d
acra5y <oli4@acrasy.net>
2019-04-20 00:47:46 +0200

    Setup eslint

--- server/index.js
+++ server/index.js
@@ -1,22 +1,24 @@
-import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import path from 'path';
+import express from "express";
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { renderToString } from "react-dom/server";
+import path from "path";
 
-import App from '../app/components/App';
-import Html from './Html';
+import App from "../app/components/App";
+import Html from "./Html";
 
 const port = 3000;
 const server = express();
 
-const assetPath = path.join(__dirname, '..', 'dist/public/');
-server.use('/public', express.static(assetPath));
+const assetPath = path.join(__dirname, "..", "dist/public/");
+server.use("/public", express.static(assetPath));
 
-server.get('/', (req, res) => {
-  const component = renderToString(<App />);
+server.get("/", (req, res) => {
+    const component = renderToString(<App />);
 
-  res.send(Html(component));
+    res.send(Html(component));
 });
 
 server.listen(port);
+// eslint-disable-next-line no-console
 console.log(`listening on port ${port}...`);
