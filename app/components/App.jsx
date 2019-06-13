f45cb8cdde6116dfd4bb380eb968ef3bad9b66d2
acra5y <oli4@acrasy.net>
2019-06-13 20:50:44 +0200

    Lint also .jsx files

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import MatrixInput from "./nDilationComputer/MatrixInput";
 
+//eslint-disable-next-line no-console
 const App = () => <MatrixInput onSubmit={console.log} />;
 
 export default App;
