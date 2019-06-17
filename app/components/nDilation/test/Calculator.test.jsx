1b3e39aee098ab5e442eafc34c6d90e3d9e64f16
acra5y <oli.schmelzle@gmail.com>
2019-06-17 23:03:32 +0200

    Fetch dilation on submit of matrix input

--- app/components/nDilation/test/Calculator.test.jsx
+++ app/components/nDilation/test/Calculator.test.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 
+import MatrixInput from "../MatrixInput";
 import Calculator from "../Calculator";
 
 describe("Calculator", () => {
@@ -8,5 +9,22 @@ describe("Calculator", () => {
         const component = shallow(<Calculator />);
 
         expect(component).toMatchSnapshot();
+    });
+
+    describe("on client", () => {
+        it("should render MatrixInput with correct mockOnSubmit handler", async () => {
+            const response = {
+                ok: true,
+                json: () => Promise.resolve({}),
+            };
+            global.window = {
+                fetch: jest.fn(() => Promise.resolve(response)),
+            };
+            const component = shallow(<Calculator />);
+
+            await component.find(MatrixInput).simulate("submit");
+
+            expect(window.fetch.mock.calls.length).toEqual(1);
+        });
     });
 });
---
+++ app/lib/test/fetchNDilation.test.js
@@ -0,0 +1,31 @@
+import fetchNDilation from "../fetchNDilation";
+
+describe("fetchNDilation", () => {
+    const fetch = jest.fn(() => Promise.resolve("whatever"));
+    const value = [1, 2, 3, 4];
+
+    it("should call fetch with correct url", () => {
+        fetchNDilation(fetch, value);
+
+        expect(fetch.mock.calls.length).toBe(1);
+        expect(fetch.mock.calls[0][0]).toEqual(
+            "http://localhost:8080/dilation"
+        );
+    });
+
+    it("should call fetch with correct body (value from params and fixed degree of 2)", () => {
+        fetchNDilation(fetch, value);
+
+        expect(fetch.mock.calls.length).toBe(1);
+        expect(JSON.parse(fetch.mock.calls[0][1].body)).toEqual({
+            value: [1, 2, 3, 4],
+            degree: 2,
+        });
+    });
+
+    it("should return whatever fetch returns", async () => {
+        const result = await fetchNDilation(fetch, value);
+
+        expect(result).toEqual("whatever");
+    });
+});
