6f3a61a18ef5603317346ce01e2847dcd1b01503
acra5y <oli4@acrasy.net>
2019-06-17 21:11:35 +0200

    Move dilation related logic to dilation directory from App

--- app/components/test/App.test.jsx
+++ app/components/test/App.test.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import App from "../App";
+import Calculator from "../nDilation/Calculator";
 
 describe("App", () => {
-    it("should render", () => {
+    it("should render Calculator", () => {
         const component = shallow(<App />);
 
-        expect(component).toMatchSnapshot();
+        expect(component.find(Calculator).exists()).toBe(true);
     });
 });
