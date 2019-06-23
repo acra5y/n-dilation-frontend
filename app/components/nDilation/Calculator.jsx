01069ece8b5d116d34dd25950fd7abcc1879ad37
acra5y <oli.schmelzle@gmail.com>
2019-06-23 21:29:25 +0200

    Refactor to useReducer

--- app/components/nDilation/Calculator.jsx
+++ app/components/nDilation/Calculator.jsx
@@ -1,38 +1,44 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 import fetchNDilation from "../../lib/fetchNDilation";
 import { useWindowContext } from "../WindowContext";
 import MatrixInput from "./MatrixInput";
 
-const createOnSubmitHandler = (
-    fetch,
-    setDilation,
-    setError
-) => async matrix => {
+const initialState = { dilation: null, error: null };
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "FETCH_SUCCESS":
+            return { dilation: action.payload, error: null };
+        case "FETCH_ERROR":
+            return { dilation: null, error: action.payload };
+        default:
+            return state;
+    }
+}
+
+const createOnSubmitHandler = (fetch, dispatch) => async matrix => {
     try {
         const response = await fetchNDilation(fetch, matrix);
 
         if (response.ok) {
             const body = await response.json();
-            setDilation(body.value);
+            dispatch({ type: "FETCH_SUCCESS", payload: body.value });
         }
     } catch (e) {
-        setDilation(null);
-        setError(e);
+        dispatch({ type: "FETCH_ERROR", payload: e });
     }
 };
 
 const Calculator = () => {
-    const [dilation, setDilation] = useState(null);
-    const [error, setError] = useState(null);
+    const [{ dilation, error }, dispatch] = useReducer(reducer, initialState);
     const window = useWindowContext();
 
     return (
         <div>
             <MatrixInput
                 onSubmit={
-                    window &&
-                    createOnSubmitHandler(window.fetch, setDilation, setError)
+                    window && createOnSubmitHandler(window.fetch, dispatch)
                 }
             />
             {dilation && <div>{JSON.stringify(dilation)}</div>}
