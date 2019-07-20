0a0832b1c0b593a03342b7309364ed2bf6edc33b
acra5y <oli.schmelzle@gmail.com>
2019-07-20 22:44:04 +0200

    Define font (with font-face to prevent flickering

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 
+import GlobalStyles from "./GlobalStyles";
 import Calculator from "./nDilation/Calculator";
 import WindowContext from "./WindowContext";
 
 const App = () => (
-    <WindowContext.Provider
-        value={typeof window !== "undefined" ? window : null}
-    >
-        <Calculator />
-    </WindowContext.Provider>
+    <Fragment>
+        <GlobalStyles />
+        <WindowContext.Provider
+            value={typeof window !== "undefined" ? window : null}
+        >
+            <Calculator />
+        </WindowContext.Provider>
+    </Fragment>
 );
 
 export default App;
--- server/Html.js
+++ server/Html.js
@@ -7,6 +7,13 @@ export default (styleTags, app) => `
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>App</title>
             ${styleTags}
+            <style>
+                @font-face {
+                    src: url('https://fonts.googleapis.com/css?family=Droid+Sans');
+                    font-family: 'Droid Sans', sans-serif;
+                    font-display: fallback;
+                }
+            </style
         </head>
         <body>
             <section id="root">${app}</section>
