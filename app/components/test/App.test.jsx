5d64763dbe61236639d6927976c276f043ee75c9
acra5y <oli4@acrasy.net>
2019-07-20 22:44:04 +0200

    Define font (with font-face to prevent flickering

--- app/components/test/App.test.jsx
+++ app/components/test/App.test.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import App from "../App";
+import GlobalStyles from "../GlobalStyles";
 import Calculator from "../nDilation/Calculator";
 
 describe("App", () => {
@@ -9,5 +10,11 @@ describe("App", () => {
         const component = shallow(<App />);
 
         expect(component.find(Calculator).exists()).toBe(true);
+    });
+
+    it("should insert global styles", () => {
+        const component = shallow(<App />);
+
+        expect(component.find(GlobalStyles).exists()).toBe(true);
     });
 });
