bc942c0a2bb9d93a1086f780c53f3702ac6aac18
acra5y <oli.schmelzle@gmail.com>
2019-07-20 22:48:26 +0200

    Add missing test case for rendering WindowContext.Provider

--- app/components/test/App.test.jsx
+++ app/components/test/App.test.jsx
@@ -3,6 +3,7 @@ import { shallow } from "enzyme";
 
 import App from "../App";
 import GlobalStyles from "../GlobalStyles";
+import WindowContext from "../WindowContext";
 import Calculator from "../nDilation/Calculator";
 
 describe("App", () => {
@@ -16,5 +17,26 @@ describe("App", () => {
         const component = shallow(<App />);
 
         expect(component.find(GlobalStyles).exists()).toBe(true);
+    });
+
+    describe("should render WindowContext Provider", () => {
+        it("on server with null as window (when window is not defined)", () => {
+            const globalWindow = global.window;
+            Reflect.deleteProperty(global, "window");
+            const component = shallow(<App />);
+
+            expect(component.find(WindowContext.Provider).prop("value")).toBe(
+                null
+            );
+            global.window = globalWindow; // restore global config to not mess with other tests
+        });
+
+        it("with global window as value", () => {
+            const component = shallow(<App />);
+
+            expect(component.find(WindowContext.Provider).prop("value")).toBe(
+                window
+            );
+        });
     });
 });
