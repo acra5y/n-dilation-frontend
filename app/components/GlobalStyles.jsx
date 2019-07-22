23e7dbf45ba39cd75e0033ba001b503ed4f6fcd8
acra5y <oli4@acrasy.net>
2019-07-22 21:31:17 +0200

    Set global font size

--- app/components/GlobalStyles.jsx
+++ app/components/GlobalStyles.jsx
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
     body {
         font-family: 'Droid Sans', sans-serif;
+        font-size: 16px;
     }
 `;
 
