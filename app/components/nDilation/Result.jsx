3babe9210944b4ea23212f58cd6f30b371ee5972
acra5y <oli.schmelzle@gmail.com>
2019-08-10 21:21:17 +0200

    Move hook related to the animated content change to higher order component

--- app/components/nDilation/Result.jsx
+++ app/components/nDilation/Result.jsx
@@ -1,47 +1,29 @@
-import React, { useEffect, useReducer } from "react";
+import React from "react";
 import styled from "styled-components";
 
+import withAnimatedContentChange from "../withAnimatedContentChange";
 import ErrorMessage from "./ErrorMessage";
 
-const animationTimeInSeconds = 0.1;
-
 const AnimatedContentTransition = styled.div`
-    transition: opacity ${animationTimeInSeconds}s ease-out;
+    transition: ${props => `opacity ${props.animationTimeInSeconds}s ease-out`};
     opacity: ${props => props.opacity};
 `;
 
-function reducer(state, action) {
-    switch (action.type) {
-        case "BLUR":
-            return { ...state, opacity: 0 };
-        case "FADE_IN_NEW_VALUE":
-            return { ...action.payload, opacity: 1 };
-        default:
-            return state;
-    }
-}
-
-const Result = props => {
-    const [{ dilation, errorDetails, opacity }, dispatch] = useReducer(
-        reducer,
-        props
-    );
-
-    useEffect(() => {
-        dispatch({ type: "BLUR" });
-        const i = setTimeout(
-            () => dispatch({ type: "FADE_IN_NEW_VALUE", payload: props }),
-            animationTimeInSeconds * 1000
-        );
-        return () => clearTimeout(i);
-    }, [props]);
-
+export const Result = ({
+    dilation,
+    errorDetails,
+    opacity,
+    animationTimeInSeconds,
+}) => {
     return (
-        <AnimatedContentTransition opacity={opacity}>
+        <AnimatedContentTransition
+            opacity={opacity}
+            animationTimeInSeconds={animationTimeInSeconds}
+        >
             {dilation && <div>{JSON.stringify(dilation, null, 1)}</div>}
             {errorDetails && <ErrorMessage errorDetails={errorDetails} />}
         </AnimatedContentTransition>
     );
 };
 
-export default Result;
+export default withAnimatedContentChange(Result);
---
+++ app/components/withAnimatedContentChange.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+import React, { useEffect, useReducer } from "react";
+import styled from "styled-components";
+
+const animationTimeInSeconds = 0.1;
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "BLUR":
+            return { ...state, opacity: 0 };
+        case "FADE_IN_NEW_VALUE":
+            return { ...state, ...action.payload, opacity: 1 };
+        default:
+            return state;
+    }
+}
+
+function withAnimatedContentChange(WrappedComponent) {
+    function WithAnimatedContentChange(props) {
+        const [state, dispatch] = useReducer(reducer, props);
+
+        useEffect(() => {
+            dispatch({ type: "BLUR" });
+            const i = setTimeout(
+                () => dispatch({ type: "FADE_IN_NEW_VALUE", payload: props }),
+                animationTimeInSeconds * 1000
+            );
+            return () => clearTimeout(i);
+        }, [props]);
+
+        return (
+            <WrappedComponent
+                {...state}
+                animationTimeInSeconds={animationTimeInSeconds}
+            />
+        );
+    }
+
+    WithAnimatedContentChange.displayName = `withAnimatedContentChange(${WrappedComponent.displayName ||
+        WrappedComponent.name})`;
+
+    return WithAnimatedContentChange;
+}
+
+export default withAnimatedContentChange;
